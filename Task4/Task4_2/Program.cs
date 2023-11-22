0755a8cc237c7cd760866a0d43a97a449b2290b5
Marco Hinojosa <MEHV291002@outlook.com>
2023-11-21 22:35:44 -0600

    #Task4.2

---
+++ Task4/Task4_2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Task4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RationalNumber rationalNumber = new RationalNumber(6, 8);
+            RationalNumber rationalNumber2 = new RationalNumber(4, 11);
+            RationalNumber rationalNumber3 = new RationalNumber(2, 6);
+            Console.WriteLine(rationalNumber.ToString());
+            Console.WriteLine($"Are Equals: {rationalNumber.Equals(rationalNumber2)}");
+            Console.WriteLine($"Are Equals: {rationalNumber.Equals(1)}\n");
+
+            List<RationalNumber> list = new List<RationalNumber>();
+            list.Add(rationalNumber);
+            list.Add(rationalNumber2);
+            list.Add(rationalNumber3);
+            list.Sort();
+            Console.WriteLine("Order by value DESC:");
+            foreach (RationalNumber number in list)
+            {
+                Console.WriteLine(number.ToString());
+            }
+
+            RationalNumber rationalNumberAdd = new RationalNumber(2, 3);
+            RationalNumber rationalNumberAdd2 = new RationalNumber(4, 5);
+            RationalNumber resultAdd = rationalNumberAdd + rationalNumberAdd2;
+            Console.WriteLine($"\nResult of add: {resultAdd.ToString()}");
+
+            RationalNumber rationalNumberSubstract = new RationalNumber(5, 6);
+            RationalNumber rationalNumberSubstract2 = new RationalNumber(3, 10);
+            RationalNumber resultSubstract = rationalNumberSubstract - rationalNumberSubstract2;
+            Console.WriteLine($"\nResult of substract: {resultSubstract.ToString()}");
+
+            RationalNumber rationalNumberMultiply = new RationalNumber(5, 6);
+            RationalNumber rationalNumberMultiply2 = new RationalNumber(4, 3);
+            RationalNumber resultMultiply = rationalNumberMultiply * rationalNumberMultiply2;
+            Console.WriteLine($"\nResult of multiply: {resultMultiply.ToString()}");
+
+            RationalNumber rationalNumberDivide = new RationalNumber(5, 6);
+            RationalNumber rationalNumberDivide2 = new RationalNumber(4, 3);
+            RationalNumber resultDivide = rationalNumberDivide / rationalNumberDivide2;
+            Console.WriteLine($"\nResult of divide: {resultDivide.ToString()}");
+
+            double rationalToDouble = (double)resultDivide;
+            Console.WriteLine($"\nRational to Double: {rationalToDouble.ToString()}");
+
+            RationalNumber intToRational = (RationalNumber)10;
+            Console.WriteLine($"\nInt to Rational: {intToRational.ToString()}");
+        }
+    }
+}
---
+++ Task4/Task4_2/RationalNumber.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_2
+{
+    internal sealed class RationalNumber : IComparable
+    {
+        public int Numerator { get; }
+
+        public byte Denominator { get; }
+
+        public RationalNumber(int numerator, byte denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new Exception();
+            }
+
+            int gcd = GetGCD(numerator, denominator);
+
+            Numerator = numerator / gcd;
+            Denominator = (byte)(denominator / gcd);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not RationalNumber || ((RationalNumber)obj).Denominator != this.Denominator || ((RationalNumber)obj).Numerator != this.Numerator)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string output = Numerator.ToString() + "/" + Denominator.ToString();
+            return output;
+        }
+
+        int GetGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        int GetLCM(int a, int b)
+        {
+            int gcd = GetGCD(a, b);
+            return Math.Abs(a * b) / gcd;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is RationalNumber)
+            {
+                double doubleNumber = (double)(RationalNumber)obj;
+                double doubleNumber2 = (double)this;
+
+                if (doubleNumber < doubleNumber2)
+                {
+                    return -1;
+                }
+                else if (doubleNumber == doubleNumber2)
+                {
+                    return 0;
+                }
+
+                return 1;
+            }
+            else
+            {
+                throw new Exception("Object is not a Rational Number");
+            }
+        }
+
+        public static RationalNumber operator +(RationalNumber number, RationalNumber other)
+        {
+            if (number.Denominator == other.Denominator)
+            {
+                var numerator = number.Numerator + other.Numerator;
+
+                return new RationalNumber(numerator, number.Denominator);
+            }
+            else
+            {
+                var denominator = number.GetLCM(number.Denominator, other.Denominator);
+                var numerator = number.Numerator * other.Denominator;
+                var numerator2 = other.Numerator * number.Denominator;
+
+                return new RationalNumber(numerator + numerator2, (byte)denominator);
+            }
+        }
+
+        public static RationalNumber operator -(RationalNumber number, RationalNumber other)
+        {
+            if (number.Denominator == other.Denominator)
+            {
+                var numerator = number.Numerator - other.Numerator;
+
+                return new RationalNumber(numerator, number.Denominator);
+            }
+            else
+            {
+                var denominator = number.GetLCM(number.Denominator, other.Denominator);
+                var numerator = number.Numerator * other.Denominator;
+                var numerator2 = other.Numerator * number.Denominator;
+
+                return new RationalNumber(numerator - numerator2, (byte)denominator);
+            }
+        }
+
+        public static RationalNumber operator *(RationalNumber number, RationalNumber other)
+        {
+            var numerator = number.Numerator * other.Numerator;
+            var denominator = number.Denominator * other.Denominator;
+
+            return new RationalNumber(numerator, (byte)denominator);
+        }
+
+        public static RationalNumber operator /(RationalNumber number, RationalNumber other)
+        {
+            var numerator = number.Numerator * other.Denominator;
+            var denominator = number.Denominator * other.Numerator;
+
+            return new RationalNumber(numerator, (byte)denominator);
+        }
+
+        public static explicit operator double(RationalNumber rational)
+        {
+            return (double)rational.Numerator / rational.Denominator;
+        }
+
+        public static explicit operator RationalNumber(int value)
+        {
+            return new RationalNumber(value, 1);
+        }
+    }
+}
